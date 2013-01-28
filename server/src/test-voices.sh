53d364edc1aa749e381dce219afc053b7e86a131
foobarlab <eyesee@foobarlab.net>
2013-01-28 10:54:07 +0100

    voices test script

---
+++ server/src/test-voices.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# script to test all configured voices
+# writes result to a temporary folder
+ 
+TEXT='hello world'
+TMPDIR=`mktemp -d`
+for i in $( ls config/voices); do
+  echo found voice-script: $i
+  echo $TEXT | config/voices/$i $TMPDIR/$i.mp3
+done
+echo written voice test files to $TMPDIR
