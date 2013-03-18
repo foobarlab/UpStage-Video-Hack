4959c6ccf64fec4efeb82a04f1c14fa49032cbb8
foobarlab <eyesee@foobarlab.net>
2013-03-18 21:54:00 +0100

    added note how to run from commandline

--- server/src/test-voices.sh
+++ server/src/test-voices.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # script to test all configured voices
 # writes result to a temporary folder
+#
+# run this script with the following command
+# to log all messages to 'test-voices.log':
+#
+# ./test-voices.sh 1> test-voices.log 2>&1
+#
 
 TEXT='hello world'
 TMPDIR=`mktemp -d`
